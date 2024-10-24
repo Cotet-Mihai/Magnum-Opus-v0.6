9cdac17e8cdf05b962b87a93bd567458a5d1ddf7
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2024-10-24 15:55:37 +0300

    Implemented functionality to dynamically update role options in a dropdown based on the selected department, ensuring relevant roles are available and a placeholder is displayed by default.

--- static/js/employees/addEmployee.js
+++ static/js/employees/addEmployee.js
@@ -1,31 +1,24 @@
 export class EmployeeFormController {
-    /**
-     * Initializes a new instance of the AddEmployee class.
-     *
-     * @param {HTMLElement} button - The button that displays the form.
-     * @param {HTMLElement} form - The form element containing the input fields.
-     * @param {Notification} notification - Notification function.
-     * @param {CardEmployee} cardEmployee - Control for card-employee.
-     */
     constructor(button, form, notification, cardEmployee) {
-
         this.button = button;
         this.form = form;
-        this.notification = notification
-        this.cardEmployee = cardEmployee
+        this.notification = notification;
+        this.cardEmployee = cardEmployee;
 
-        this.formElements = form.querySelectorAll('input, select, textarea, button');
+        this.formElements = this.form.querySelectorAll('input, select, textarea, button');
 
-        this.firstName = form.querySelector('#add-employee-form-first-name');
-        this.lastName = form.querySelector('#add-employee-form-last-name');
-        this.department = form.querySelector('#add-employee-form-department');
-        this.role = form.querySelector('#add-employee-form-role');
-        this.date = form.querySelector('#add-employee-form-date');
-        this.county = form.querySelector('#add-employee-form-county');
-        this.phone = form.querySelector('#add-employee-form-phone');
+        this.firstName = this.form.querySelector('#add-employee-form-first-name');
+        this.lastName = this.form.querySelector('#add-employee-form-last-name');
+        this.department = this.form.querySelector('#add-employee-form-department');
+        this.role = this.form.querySelector('#add-employee-form-role');
+        this.date = this.form.querySelector('#add-employee-form-date');
+        this.county = this.form.querySelector('#add-employee-form-county');
+        this.phone = this.form.querySelector('#add-employee-form-phone');
 
-        this.setCurrentDate();  // Set current date for inputDate
-        this.disableElements();  // Disable all form elements
+        this.setCurrentDate();
+        this.disableElements();
+
+        this.syncSelectOptionsBetweenDepartmentAndRole();
 
         this.setupEventListeners();
     }
@@ -33,82 +26,87 @@ export class EmployeeFormController {
     setupEventListeners() {
         this.button.addEventListener('click', () => this.updateFormSizeAndState());
         this.form.addEventListener('submit', (event) => this.addNewEmployee(event));
+        this.department.addEventListener('change', () => this.syncSelectOptionsBetweenDepartmentAndRole());
     }
 
-    /**
-     * Sets the current date in the input of type date.
-     */
     setCurrentDate() {
         this.date.value = new Date().toISOString().split('T')[0];
     }
 
-    /**
-     * Disables all input elements within the form.
-     */
     disableElements() {
         this.formElements.forEach(element => element.disabled = true);
     }
 
-    /**
-     * Enables all input elements within the form.
-     */
     enableElements() {
         this.formElements.forEach(element => element.disabled = false);
     }
 
-    /**
-     * Resets the values of form elements to their default state.
-     * - For SELECT elements, it resets to the first option.
-     * - For input of type date, it sets the current date.
-     * - For other input types, it clears the value.
-     */
     resetElementsValue() {
         this.formElements.forEach(element => {
             if (element.type !== 'button') {
                 if (element.tagName === 'SELECT') {
-                    element.selectedIndex = 0; // Reset selection
+                    element.selectedIndex = 0;
                 } else if (element.type === 'date') {
-                    this.setCurrentDate(); // Set current date for date input
+                    this.setCurrentDate();
                 } else {
-                    element.value = ''; // Clear value for other inputs
+                    element.value = '';
                 }
             }
         });
+        this.syncSelectOptionsBetweenDepartmentAndRole();
     }
 
-    /**
-     * Updates the height of the form and toggles the state of its elements.
-     *
-     * If the current height of the form is empty or '0px', it sets the height to
-     * '100px' and enables the form elements. If the height is anything else,
-     * it collapses the form to '0px', disables the form elements after a
-     * brief delay, and resets their values to the default state.
-     */
+    syncSelectOptionsBetweenDepartmentAndRole() {
+        const options = {
+            IT: [
+                { value: 'Manager', text: 'Manager' },
+                { value: 'Suport', text: 'Suport' },
+                { value: 'Tehnic', text: 'Tehnic' }
+            ],
+            Operational: [
+                { value: 'Director Operational', text: 'Director Operational' },
+                { value: 'Manager Regional', text: 'Manager Regional' },
+                { value: 'Manager Zonal', text: 'Manager Zonal' },
+                { value: 'Manager Local', text: 'Manager Local' },
+                { value: 'Operator', text: 'Operator' }
+            ]
+        };
+
+        this.populateSelectOptions(this.role, options[this.department.value] || []);
+    }
+
+    populateSelectOptions(selectElement, options) {
+        selectElement.innerHTML = '';
+        const placeholder = document.createElement('option');
+        placeholder.value = '';
+        placeholder.textContent = 'Rol';
+        placeholder.disabled = true;
+        placeholder.selected = true;
+        selectElement.appendChild(placeholder);
+
+        options.forEach(option => {
+            const newOption = document.createElement('option');
+            newOption.value = option.value;
+            newOption.textContent = option.text;
+            selectElement.appendChild(newOption);
+        });
+    }
+
     updateFormSizeAndState() {
         if (this.form.style.height === '' || this.form.style.height === '0px') {
             this.form.style.height = '100px';
-            this.enableElements(); // Enable the form elements when expanded
+            this.enableElements();
         } else {
             this.form.style.height = '0px';
             setTimeout(() => {
-                this.disableElements();  // Disable elements
-                this.resetElementsValue(); // Reset values of the form elements
+                this.disableElements();
+                this.resetElementsValue();
             }, 500);
-
         }
-}
+    }
 
-    /**
-     * Adds a new employee by sending a POST request to the server.
-     *
-     * @param {Event} event - The event object representing the form submission.
-     * @returns {void} - This function does not return a value.
-     *
-     * @throws {Error} - Throws an error if the network response is not ok or if the server
-     *                  returns an error message.
-     */
     addNewEmployee(event) {
-        event.preventDefault();  // Prevent the form from submitting normaly
+        event.preventDefault();
 
         fetch('employees/add', {
             method: 'POST',
--- static/js/employees/cardEmployee.js
+++ static/js/employees/cardEmployee.js
@@ -1,5 +1,13 @@
 export class CardEmployee {
-
+    /**
+     * Initializes a new instance of the CardEmployee class.
+     *
+     * @param {HTMLElement} filterBy - The filter element for employee attributes.
+     * @param {HTMLElement} filterRole - The filter element for employee roles.
+     * @param {HTMLElement} searchBar - The search input element for employee names.
+     * @param {HTMLElement} employeesContainer - The container element where employee cards are displayed.
+     * @param {Notification} notification - The notification handler for displaying messages.
+     */
     constructor(filterBy, filterRole, searchBar, employeesContainer, notification) {
         this.filterBy = filterBy;
         this.filterRole = filterRole;
@@ -12,13 +20,18 @@ export class CardEmployee {
         this.setupEventListeners();  // Set up event listeners for filtering
     }
 
+    /**
+     * Sets up event listeners for the filter elements and search bar.
+     */
     setupEventListeners() {
         this.filterBy.addEventListener('change', this.filterEmployees.bind(this));
         this.filterRole.addEventListener('change', this.filterEmployees.bind(this));
         this.searchBar.addEventListener('input', this.filterEmployees.bind(this));
     }
 
-
+    /**
+     * Fetches filtered employees from the server and updates the employee container.
+     */
     filterEmployees() {
         fetch('employees/filter', {
             method: 'POST',
@@ -42,16 +55,26 @@ export class CardEmployee {
             .catch(error => this.notification.showNotification('Error', error.message));  // Show error notification
     }
 
+    /**
+     * Initializes FullCardEmployee instances for each employee card in the container.
+     */
     setFullCard() {
         this.employeeCard = this.employeesContainer.querySelectorAll('.full-card-employee');
 
         this.employeeCard.forEach(card => {
             new FullCardEmployee(card, this.notification, this.filterEmployees.bind(this));
-        })
+        });
     }
 }
 
 class FullCardEmployee {
+    /**
+     * Initializes a new instance of the FullCardEmployee class.
+     *
+     * @param {HTMLElement} card - The employee card element.
+     * @param {Notification} notification - The notification handler for displaying messages.
+     * @param {Function} filter - The filter function to call after editing or deleting an employee.
+     */
     constructor(card, notification, filter) {
         this.card = card;
         this.notification = notification;
@@ -68,27 +91,86 @@ class FullCardEmployee {
 
         this.allInputs = card.querySelectorAll('.edit-input');
 
+        this.department = card.querySelector('#edit-departmnet');
+        this.role = card.querySelector('#edit-role');
+
         this.setupEventListeners();
+
+        this.syncSelectOptionsBetweenDepartmentAndRole();
     }
 
+    /**
+     * Sets up event listeners for the edit, delete, and save buttons.
+     */
     setupEventListeners() {
         this.editButton.addEventListener('click', () => this.updateFormSizeAndState());
         this.deleteButton.addEventListener('click', () => this.deleteEmployee());
         this.saveButton.addEventListener('click', () => this.editEmployee());
+        this.department.addEventListener('change', () => this.syncSelectOptionsBetweenDepartmentAndRole());
     }
 
+    /**
+     * Enables the input fields and buttons for editing an employee.
+     */
     enableElements() {
         this.allInputs.forEach(input => input.disabled = false);
         this.saveButton.disabled = false;
         this.deleteButton.disabled = false;
     }
 
+    /**
+     * Disables the input fields and buttons for editing an employee.
+     */
     disableElements() {
         this.allInputs.forEach(input => input.disabled = true);
         this.saveButton.disabled = true;
         this.deleteButton.disabled = true;
     }
 
+    syncSelectOptionsBetweenDepartmentAndRole() {
+        const optionsIT = [
+            { value: 'Manager', text: 'Manager' },
+            { value: 'Suport', text: 'Suport' },
+            { value: 'Tehnic', text: 'Tehnic'}
+        ];
+
+        const optionsOperational = [
+            { value: 'Director Operational', text: 'Director Operational'},
+            { value: 'Manager Regional', text: 'Manager Regional'},
+            { value: 'Manager Zonal', text: 'Manager Zonal'},
+            { value: 'Manager Local', text: 'Manager Local'},
+            { value: 'Operator', text: 'Operator'}
+        ]
+
+        this.role.innerHTML = '';
+
+        const placeholder = document.createElement('option');
+        placeholder.value = '';
+        placeholder.textContent = 'Rol';
+        placeholder.disabled = true;
+        placeholder.selected = true;
+        this.role.appendChild(placeholder);
+
+        if (this.department.value === 'IT') {
+            optionsIT.forEach(option => {
+                const newOption = document.createElement('option');
+                newOption.value = option.value;
+                newOption.textContent = option.text;
+                this.role.appendChild(newOption);
+            })
+        } else if (this.department.value === 'Operational') {
+            optionsOperational.forEach(option => {
+                const newOption = document.createElement('option');
+                newOption.value = option.value;
+                newOption.textContent = option.text;
+                this.role.appendChild(newOption);
+            })
+        }
+    }
+
+    /**
+     * Resets the values of the input fields in the employee edit form.
+     */
     resetElementsValue() {
         this.allInputs.forEach(element => {
             if (element.tagName === 'SELECT') {
@@ -97,8 +179,12 @@ class FullCardEmployee {
                 element.value = ''; // Clear value for other inputs
             }
         });
+        this.syncSelectOptionsBetweenDepartmentAndRole();
     }
 
+    /**
+     * Toggles the size and state of the edit container for the employee.
+     */
     updateFormSizeAndState() {
         if (this.editContainer.style.width === '0px' || this.editContainer.style.width === '') {
             this.editContainer.style.width = '250px';
@@ -112,20 +198,23 @@ class FullCardEmployee {
         }
     }
 
+    /**
+     * Deletes the employee after user confirmation and updates the employee list.
+     */
     deleteEmployee() {
-        const confirmation = confirm("Sigur vrei sa stergi acest utilizator ?")
+        const confirmation = confirm("Sigur vrei sa stergi acest utilizator ?");
 
         if (confirmation) {
             fetch('employees/delete', {
                 method: 'DELETE',
-                headers: {'Content-type' : 'application/json'},
+                headers: { 'Content-type': 'application/json' },
                 body: JSON.stringify({
                     userID: this.userID
                 })
             })
                 .then(response => {
                     if (!response.ok) {
-                        throw new Error('Network response was not ok')
+                        throw new Error('Network response was not ok');
                     }
                     return response.json();
                 })
@@ -134,13 +223,15 @@ class FullCardEmployee {
                     this.filter();
                 })
                 .catch(error => {
-                    this.notification.showNotification('Error', error.message)
-                })
+                    this.notification.showNotification('Error', error.message);
+                });
         }
     }
 
+    /**
+     * Edits the employee details and updates the employee list after confirmation.
+     */
     editEmployee() {
-
         const lastName = this.card.querySelector('#edit-last-name');
         const firstName = this.card.querySelector('#edit-first-name');
         const department = this.card.querySelector('#edit-departmnet');
@@ -154,7 +245,7 @@ class FullCardEmployee {
         if (confirmation) {
             fetch('employees/edit', {
                 method: 'PUT',
-                headers: {'Content-type': 'application/json'},
+                headers: { 'Content-type': 'application/json' },
                 body: JSON.stringify({
                     ID: this.userID,
                     lastName: lastName.value,
@@ -168,7 +259,7 @@ class FullCardEmployee {
             })
             .then(respone => {
                 if (!respone.ok) {
-                    throw new Error('Network response was not ok')
+                    throw new Error('Network response was not ok');
                 }
                 return respone.json();
             })
@@ -180,9 +271,8 @@ class FullCardEmployee {
                     }, 700);
                 })
                 .catch(error => {
-                    this.notification.showNotification('Error', error.message)
-                })
+                    this.notification.showNotification('Error', error.message);
+                });
         }
     }
-
 }
